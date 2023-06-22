75c3487453026636b94a9a8e2e451f35df79ee35
Dipankar Halder <dipankar15-11137@diu.edu.bd>
2023-06-22 14:35:14 +0600

    show review

--- src/Components/Pages/Home/OurServices/Review/ShowReview.js
+++ src/Components/Pages/Home/OurServices/Review/ShowReview.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import StarRating from './StarRating';
 
 const ShowReview = ({ review }) => {
-  console.log(review);
+  const rating = review?.rating;
+  console.log(rating);
   return (
-    <div>
-      <h1>Show</h1>
+    <div className="p-3">
+      <h1>{review?.review}</h1>
+      <div className="flex justify-between items-center">
+        <StarRating rating={rating} />
+        <div className="mr-4 flex items-center font-bold">
+          <img
+            className="h-8 mr-3"
+            src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7914927/man-icon-md.png"
+            alt=""
+          />
+          <h1>{review?.userName}</h1>
+        </div>
+      </div>
     </div>
   );
 };
