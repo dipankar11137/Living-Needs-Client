4e594e022ebb91e6dc4cfca88dbc6c6528493b79
Dipankar Halder <dipankar15-11137@diu.edu.bd>
2023-06-22 14:18:55 +0600

    show review half

--- src/Components/Pages/Home/OurServices/Review/ShowReview.js
+++ src/Components/Pages/Home/OurServices/Review/ShowReview.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-const ShowReview = () => {
+const ShowReview = ({ review }) => {
+  console.log(review);
   return (
     <div>
       <h1>Show</h1>
--- src/Components/Pages/Home/OurServices/Technician.js
+++ src/Components/Pages/Home/OurServices/Technician.js
@@ -8,91 +8,103 @@ const Technician = ({ service }) => {
   const [arrow, setArrow] = useState(false);
   const [review, setReview] = useState(false);
   const navigator = useNavigate();
+  const [reviews, setReviews] = useState([]);
+
+  const handleArrow = email => {
+    // console.log(email);
+    setArrow(false);
+    const url = `http://localhost:5000/review/${email}`;
+    fetch(url)
+      .then(res => res.json())
+      .then(data => setReviews(data));
+  };
+
   const handleBook = id => {
     navigator(`/bookService/${id}`);
   };
 
-return (
-  <div>
-    <div className="card bg-base-100 w-11/12 text-black shadow-2xl   hover:shadow-inner">
-      <figure>
-        <img
-          className="w-full pic-style"
-          src={service?.img}
-          alt=""
-          style={{ height: '300px' }}
-        />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title font-bold text-2xl">{service?.name}</h2>
-        <p>{service?.location}</p>
-        <h2 className="mt-2">
-          Salary : <span className="font-bold">{service?.price} BDT</span> / Per
-          Day
-        </h2>
-        <p>{service.description}</p>
-        <p>Phone Number : {service?.number}</p>
-      </div>
-      <div className="mb-2 p-2 flex justify-between">
-        <div>
-          {review ? (
+  return (
+    <div>
+      <div className="card bg-base-100 w-11/12 text-black shadow-2xl   hover:shadow-inner">
+        <figure>
+          <img
+            className="w-full pic-style"
+            src={service?.img}
+            alt=""
+            style={{ height: '300px' }}
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title font-bold text-2xl">{service?.name}</h2>
+          <p>{service?.location}</p>
+          <h2 className="mt-2">
+            Salary : <span className="font-bold">{service?.price} BDT</span> /
+            Per Day
+          </h2>
+          <p>{service.description}</p>
+          <p>Phone Number : {service?.number}</p>
+        </div>
+        <div className="mb-2 p-2 flex justify-between">
+          <div>
+            {review ? (
+              <button
+                onClick={() => setReview(false)}
+                className="btn font-bold btn-secondary text-white"
+              >
+                minimize Review{' '}
+              </button>
+            ) : (
+              <button
+                onClick={() => setReview(true)}
+                className="btn font-bold btn-primary text-white"
+              >
+                Add Review{' '}
+              </button>
+            )}
             <button
-              onClick={() => setReview(false)}
-              className="btn font-bold btn-secondary text-white"
+              onClick={() => handleBook(service._id)}
+              className="btn font-bold ml-20 text-white"
             >
-              minimize Review{' '}
+              book now{' '}
+            </button>
+          </div>
+          {arrow ? (
+            <button onClick={() => handleArrow(service?.email)}>
+              <BiUpArrowCircle className="text-4xl font-bold mr-4 text-primary" />
             </button>
           ) : (
-            <button
-              onClick={() => setReview(true)}
-              className="btn font-bold btn-primary text-white"
-            >
-              Add Review{' '}
+            <button onClick={() => setArrow(true)}>
+              <BiDownArrowCircle className="text-4xl font-bold mr-4 text-primary" />
             </button>
           )}
-          <button
-            onClick={() => handleBook(service._id)}
-            className="btn font-bold ml-20 text-white"
-          >
-            book now{' '}
-          </button>
         </div>
-        {arrow ? (
-          <button onClick={() => setArrow(false)}>
-            <BiUpArrowCircle className="text-4xl font-bold mr-4 text-primary" />
-          </button>
-        ) : (
-          <button onClick={() => setArrow(true)}>
-            <BiDownArrowCircle className="text-4xl font-bold mr-4 text-primary" />
-          </button>
-        )}
+        {/* add review */}
+        <div>
+          {review ? (
+            <div>
+              <AddReview email={service?.email} setReview={setReview} />
+            </div>
+          ) : (
+            <></>
+          )}
+        </div>
+        {/* show review */}
+        <div>
+          {arrow ? (
+            <div>
+              {reviews.map(review => (
+                <ShowReview key={review._id} review={review}></ShowReview>
+              ))}
+            </div>
+          ) : (
+            <></>
+          )}
+        </div>
       </div>
-      {/* add review */}
-      <div>
-        {review ? (
-          <div>
-            <AddReview email={service?.email} setReview={setReview} />
-          </div>
-        ) : (
-          <></>
-        )}
-      </div>
-      {/* show review */}
-      <div>
-        {arrow ? (
-          <div>
-            <h1>Hello</h1>
-            <ShowReview />
-          </div>
-        ) : (
-          <></>
-        )}
-      </div>
-    </div>
 
-    {/* review */}
-  </div>
-);
+      {/* review */}
+    </div>
+  );
 };
 
 export default Technician;
